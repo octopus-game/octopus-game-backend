f4e1f4a2b69def24f496a213b2a4888b5e41acf6
Andy Wood <andy@bitshaper.co.uk>
2024-07-01 14:55:04 +0100

    Add script header and fix typos to get data

--- carbon_intensity_api.py
+++ carbon_intensity_api.py
@@ -1,17 +1,17 @@
+#!/usr/bin/env python3
+
 import httpx
-from datetime import datetime 
+from datetime import datetime, timezone
 
 def get_carbon_intensity_data(postcode=None, region_id=13):
     # If no postcode is passed, data for London regions is returned.
     headers = {'Accept': 'application/json'}
     client = httpx.Client()
     midnight = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
-
-
     if postcode is None:
-        request = client.get('https://api.carbonintensity.org.uk/regional/intensity/{midnight}/fw24h/regional/regionid/{regionid}', params={}, headers = headers)
+        request = client.get(f"https://api.carbonintensity.org.uk/regional/intensity/{midnight.isoformat()}/fw24h/regionid/{region_id}", params={}, headers = headers)
         
     
-    print (request.json())
+    print(request.json())
 
 get_carbon_intensity_data()
