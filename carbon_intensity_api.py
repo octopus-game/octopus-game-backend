8ac8d76911d74754e549baa90c3a62e7ac1a1792
SAHANA VENKATESH <sahanavenkatesh@SAHANAs-MacBook-Pro.local>
2024-07-01 20:28:24 +0100

    merge and get aggregated data

--- carbon_intensity_api.py
+++ carbon_intensity_api.py
@@ -3,9 +3,16 @@
 import httpx
 from datetime import datetime, timezone, timedelta
 import numpy as np
-from tariffs import date_to_index
+from tariffs import date_to_index, get_tariff_data
 from collections import Counter
+from rich import print
 
+'''
+Usage
+from carbon_intensity_api import get_aggregate_carbon_intensity_tariff_data
+answer_dict = get_aggregate_carbon_intensity_tariff_data() 
+Note : Currently we get the data for London
+'''
 def parse_generationmix(generationmix):
     fuel_list = []
     perc_list = []
@@ -23,46 +30,46 @@ def get_carbon_intensity_data(regionid=13):
     headers = {'Accept': 'application/json'}
     client = httpx.Client()
     midnight = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
-    # 12 AM TO 12 AM  
-    request = client.get(f"https://api.carbonintensity.org.uk/regional/intensity/{midnight.isoformat()}/fw24h/regionid/{regionid}", params={}, headers = headers)
-    intensity_data_today = request.json().get('data').get('data') # its of the format data "{metadata, data :{}}"
-
-    # Tomorrow 12 AM to 3AM 
     tomorrow_start =  midnight + timedelta(days=1)
     tomorrow_end = tomorrow_start + timedelta(hours=3)
-    request = client.get(f"https://api.carbonintensity.org.uk/regional/intensity/{tomorrow_start.isoformat()}/{tomorrow_end.isoformat()}/regionid/{regionid}", params={}, headers = headers)
-    intensity_data_tomorrow = request.json().get('data').get('data')
+    request = client.get(f"https://api.carbonintensity.org.uk/regional/intensity/{midnight.isoformat()}/{tomorrow_end.isoformat()}/regionid/{regionid}", params={}, headers = headers)
+    intensity_data = request.json().get('data').get('data')
 
 
     by_half_hour = [None] * 54
-    for day in [(0,intensity_data_today), (48,intensity_data_tomorrow)]:
-        buffer = day[0]
-        for block in day[1]:
-            index = date_to_index(block['from'])
-            intensity_value = max(block['intensity'].get('forecast',0), block['intensity'].get('actual',0))
-            intensity_index = block['intensity'].get('index',None)
-            top_3_generation_mix = parse_generationmix(block['generationmix'])
-            if index is not None:
-                if buffer > 0 and index < 47:
-                    index += buffer # Early hours of tomorrow 
-                by_half_hour[index] = {'intensity_value':intensity_value,'intensity_index':intensity_index,'top_3_generation_mix': top_3_generation_mix}
+    for block in intensity_data:
+        index = date_to_index(block['from'])
+        intensity_value = max(block['intensity'].get('forecast',0), block['intensity'].get('actual',0))
+        intensity_index = block['intensity'].get('index',None)
+        top_3_generation_mix = parse_generationmix(block['generationmix'])
+        if index is not None:
+            date = datetime.fromisoformat(block['from'])
+            midnight = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
+            from_midnight = date - midnight
+            buffer = from_midnight.days*48
+            by_half_hour[index+buffer] = {'intensity_value':intensity_value,'intensity_index':intensity_index,'top_3_generation_mix': top_3_generation_mix}
     return by_half_hour
 
-def get_aggregate_carbon_intensity_tariff_data(regionid=13):
+def get_aggregate_carbon_intensity_tariff_data(regionid=13,region_code='C'): # Both point to London at the moment
     aggregate_carbon_intensity_tariff_data = {}
     carbonintensity = get_carbon_intensity_data(regionid=regionid)
+    tariff_data = get_tariff_data(region_code=region_code)
     morning, afternoon, evening, night = [],[],[],[]
     time_of_day_indexes = {'morning' : (14,24), 'afternoon': (24,34) , 'evening': (34,44) , 'night': (44,54)}
+    max_tariff = 0
+    peak_time_of_day = None
     for time_of_day, indexes in time_of_day_indexes.items():
         count = 0
         intensity_sum = 0
         intensity_index_list = []
         energy_source_list = []
         energy_contribution_sum = 0
+        tariff_sum = 0
 
         for i in range(indexes[0],indexes[1]):
             intensity_sum += carbonintensity[i]['intensity_value']
             intensity_index_list.append(carbonintensity[i]['intensity_index'])
+            tariff_sum+=tariff_data[i]
             if carbonintensity[i]['top_3_generation_mix'][0]['fuel'] != 'imports': # Interest in generated power rather than importsS
                 energy_source_list.append(carbonintensity[i]['top_3_generation_mix'][0]['fuel'])
                 energy_contribution_sum += carbonintensity[i]['top_3_generation_mix'][0]['perc']
@@ -72,14 +79,21 @@ def get_aggregate_carbon_intensity_tariff_data(regionid=13):
             count +=1
 
         average_intensity_value = intensity_sum / count 
+        average_tariff_cost = tariff_sum / count
         common_intensity_index = Counter(intensity_index_list).most_common(1)[0][0]
         common_energy_source = Counter(energy_source_list).most_common(1)[0][0]
         common_energy_source_contribution = int(energy_contribution_sum / count)
         aggregate_carbon_intensity_tariff_data[time_of_day] = {'average_carbon_intensity_value(gCO2/kWh)':average_intensity_value, 'common_intensity_index':common_intensity_index,
-                                                                 'common_energy_source':common_energy_source, 'common_energy_source_contribution(%)': common_energy_source_contribution }
+                                                                 'common_energy_source':common_energy_source, 'common_energy_source_contribution(%)': common_energy_source_contribution,
+                                                                 'average_tariff':average_tariff_cost, 'peak': False }
+        if average_tariff_cost > max_tariff:
+            peak_time_of_day = time_of_day
+            max_tariff = average_tariff_cost
+
+    aggregate_carbon_intensity_tariff_data[peak_time_of_day]['peak'] = True
     return aggregate_carbon_intensity_tariff_data
 
-
+print(get_aggregate_carbon_intensity_tariff_data())
 
 
     
--- tariffs.py
+++ tariffs.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from rich import print
 import os
 import httpx
 from datetime import datetime, timezone, timedelta
@@ -13,28 +12,29 @@ def date_to_index(iso_date):
   return int(from_midnight.seconds / 60 / 30)
 
 
-def get_tariff_data():
+def get_tariff_data(region_code='C'):
     api_key = os.environ['OCTOPUS_API_KEY']
     auth = httpx.BasicAuth(username=api_key, password="")
     client = httpx.Client()
     midnight = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
     tomorrow_start =  midnight + timedelta(days=1)
     tomorrow_end = tomorrow_start + timedelta(hours=3)
-    tariff_url = f"https://api.octopus.energy/v1/products/AGILE-FLEX-22-11-25/electricity-tariffs/E-1R-AGILE-FLEX-22-11-25-C/standard-unit-rates/"
-    response = client.get(tariff_url, auth=auth)
+    tariff_url = f"https://api.octopus.energy/v1/products/AGILE-FLEX-22-11-25/electricity-tariffs/E-1R-AGILE-FLEX-22-11-25-{region_code}/standard-unit-rates/"
+    response = client.get(tariff_url,params={"period_from": midnight.isoformat(), "period_to": tomorrow_end.isoformat()},auth=auth)
     # Type of Contract :  Cap stays at £1 per unit but new formula only deducts 17.9p from higher unit prices, https://energy-stats.uk/octopus-agile-east-midlands/
-    # Tariff code is of the format T"E-1R-$PRODUCT_CODE-C" where PRODUCT_CODE here is AGILE-FLEX-22-11-25 and C is region
+    # Tariff code is of the format T"E-1R-$PRODUCT_CODE-C" where PRODUCT_CODE here is AGILE-FLEX-22-11-25 and C is region (London)
     # To change region, please use https://en.m.wikipedia.org/wiki/Meter_Point_Administration_Number
 
-    print(response)
     data = response.json()
     results = data.get('results')
-    by_half_hour = [None] * 48
+    by_half_hour = [None] * 54
 
     for block in results:
         index = date_to_index(block['valid_from'])
+        date = datetime.fromisoformat(block['valid_from'])
+        midnight = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
+        from_midnight = date - midnight
+        buffer = from_midnight.days*48
         if index is not None:
-            by_half_hour[index] = block['value_inc_vat']
+            by_half_hour[buffer+index] = block['value_inc_vat']
     return by_half_hour
-
-print(get_tariff_data())
